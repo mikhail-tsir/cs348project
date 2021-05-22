59aa0823f69f9ada244da2870dcc6e211293426f
mikhail-tsir <mtsirlin@uwaterloo.ca>
2021-05-22 15:12:56 -0400

    refactor

---
+++ db/init.sql
@@ -0,0 +1,14 @@
+CREATE TABLE users(
+    user_id INT NOT NULL AUTO_INCREMENT,
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY(user_id)
+);
+
+INSERT INTO users (first_name, last_name)
+VALUES
+    ('Mikhail', 'Tsirlin'),
+    ('Henry', 'Chen'),
+    ('Daniel', 'Pechersky'),
+    ('Dylan', 'Shortt');
