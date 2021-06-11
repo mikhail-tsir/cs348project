68c7612d97919df886cfbde89b3b4f8267cfad82
mikhail-tsir <mtsirlin@uwaterloo.ca>
2021-06-11 17:28:22 -0400

    fix login forms and refactor ui

--- frontend/index.js
+++
@@ -1,7 +0,0 @@
-function authenticate() {
-    // Pushing ID to local storage
-    var tokenId = document.getElementById("frm1").elements[0].value;
-    localStorage.setItem("tokenId", tokenId);
-    console.log("tokenId: ",  tokenId);    
-}
-
