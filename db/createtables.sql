30e5433d3fc5f229133a9585f448342ec49afd6f
Sky <38119981+XuezhongYe@users.noreply.github.com>
2021-06-11 23:11:14 -0400

    Update createtables.sql

--- db/createtables.sql
+++ db/createtables.sql
@@ -1,8 +1,10 @@
+
+
 CREATE TABLE Account
   ( 
 	id     			DECIMAL(9, 0) NOT NULL PRIMARY KEY, 
-     	email    		VARCHAR(30) NOT NULL, 
-     	account_password	VARCHAR(25) NOT NULL
+	email    		VARCHAR(30) NOT NULL, 
+	account_password	VARCHAR(25) NOT NULL
   ); 
 
 CREATE TABLE Company 
@@ -10,7 +12,7 @@ CREATE TABLE Company
 	id 		DECIMAL(9, 0) NOT NULL,
 	PRIMARY KEY(id), 
 	FOREIGN KEY(id) REFERENCES Account(id),
-     	company_name   	VARCHAR(30) NOT NULL
+	company_name   	VARCHAR(30) NOT NULL
   ); 
 
 CREATE TABLE Job_seeker 
@@ -18,33 +20,35 @@ CREATE TABLE Job_seeker
 	id 		DECIMAL(9, 0) NOT NULL,
 	PRIMARY KEY(id), 
 	FOREIGN KEY(id) REFERENCES Account(id),
-     	name   		VARCHAR(30) NOT NULL
+	name   		VARCHAR(30) NOT NULL
   ); 
+
   
 CREATE TABLE Job 
   ( 	
-	job_id     	VARCHAR(40) NOT NULL PRIMARY KEY, 
+	job_id     DECIMAL(9, 0) NOT NULL PRIMARY KEY, 
 	jname		VARCHAR(20) NOT NULL, 
 	company_id 	DECIMAL(9, 0) NOT NULL,
 	description     VARCHAR(10), 
 	apply_deadline  VARCHAR(20), 
-	FOREIGN KEY(company_id) REFERENCES Company(company_id) 
+	FOREIGN KEY(company_id) REFERENCES Company(id) 
   ); 
 
 CREATE TABLE Skill 
   ( 	
-	skill_id     	VARCHAR(40) NOT NULL PRIMARY KEY, 
+	skill_id     	DECIMAL(9, 0) NOT NULL PRIMARY KEY, 
 	sname		VARCHAR(20) NOT NULL
   ); 
 
 CREATE TABLE Application 
   ( 
      skill_id  DECIMAL(9, 0) NOT NULL, 
-     job_id  DECIMAL(9, 0) NOT NULL, 
+     job_id DECIMAL(9, 0) NOT NULL, 
      PRIMARY KEY(skill_id, job_id), 
      FOREIGN KEY(skill_id) REFERENCES Job_seeker(id), 
      FOREIGN KEY(job_id) REFERENCES Job(job_id) 
   ); 
+
 
 CREATE TABLE Job_seeker_skill 
   ( 
@@ -60,7 +64,6 @@ CREATE TABLE Job_seeker_skill
      skill_id  DECIMAL(9, 0) NOT NULL, 
      job_id  DECIMAL(9, 0) NOT NULL, 
      PRIMARY KEY(skill_id, job_id), 
-     FOREIGN KEY(skill_id) REFERENCES Skill(id), 
+     FOREIGN KEY(skill_id) REFERENCES Skill(skill_id), 
      FOREIGN KEY(job_id) REFERENCES Job(job_id)
   ); 
-  
