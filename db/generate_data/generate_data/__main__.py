5850deb5e48d4aeb71cd4c95a4ae8ca16b39043e
mikhail-tsir <mtsirlin@uwaterloo.ca>
2021-07-03 11:29:51 -0400

    add delete statements in generate_data

--- db/generate_data/generate_data/__main__.py
+++ db/generate_data/generate_data/__main__.py
@@ -1,16 +1,39 @@
 from . import generate_company_account, generate_job_seeker_account, add_account_to_db, all_skills
 from itertools import chain
+from time import sleep
 import os
 
 import pymysql
+from pymysql.constants import CLIENT
 
 password = os.environ["MYSQL_ROOT_PASSWORD"]
 database = os.environ["MYSQL_DATABASE"]
 
-with pymysql.connect(host='db', user='root', password=password, database=database) as connection:
-    with connection.cursor() as cursor:
-        cursor.executemany("INSERT INTO skill (sname) VALUES (%s)", all_skills)
+retry_count = 5
 
-        for account in chain((generate_company_account() for _ in range(500)), (generate_job_seeker_account() for _ in range(800))):
-            add_account_to_db(account, cursor)
-    connection.commit()
+while True:
+    try:
+        with pymysql.connect(host="db", user="root", password=password, database=database, client_flag=CLIENT.MULTI_STATEMENTS) as connection:
+            with connection.cursor() as cursor:
+                cursor.execute(
+                    """DELETE FROM job_skill_requirements WHERE 1 = 1;
+                    DELETE FROM job_seeker_skill WHERE 1 = 1;
+                    DELETE FROM application WHERE 1 = 1;
+                    DELETE FROM skill WHERE 1 = 1;
+                    DELETE FROM job WHERE 1 = 1;
+                    DELETE FROM job_seeker WHERE 1 = 1;
+                    DELETE FROM company WHERE 1 = 1;
+                    DELETE FROM account WHERE 1 = 1;
+                    """
+                )
+                cursor.executemany("INSERT INTO skill (sname) VALUES (%s)", all_skills)
+
+                for account in chain((generate_company_account() for _ in range(500)), (generate_job_seeker_account() for _ in range(800))):
+                    add_account_to_db(account, cursor)
+            connection.commit()
+    except pymysql.err.OperationalError as e:
+        if retry_count < 0:
+            raise e
+        
+        sleep(5)
+        retry_count -= 1
