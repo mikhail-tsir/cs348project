98388db85b0dd74b904a2f5cb272a2d967202184
mikhail-tsir <mtsirlin@uwaterloo.ca>
2021-07-04 23:59:25 -0400

    add instructions for henry

--- app/index.py
+++ app/index.py
@@ -6,7 +6,33 @@ base = Blueprint("base", __name__)
 
 @base.route("/")
 def index():
+    # for henry: comment this out
     return render_template("index.html")
+
+    # uncomment this:
+
+    # from app import db
+    # with db.connect() as conn, conn.cursor() as cursor:
+    #     queries = [
+    #         """
+    #         CONTENTS OF QUERY 1
+    #         """,
+    #         ...,
+    #         """
+    #         CONTENTS OF QUERY N
+    #         """
+    #     ]
+
+    #     res = ""
+    #     for query in queries:
+    #         cursor.execute(query)
+    #         for row in cursor.fetchall():
+    #             res += str(row) + "\n"
+    #         res += "\n"
+        
+    #     return res
+
+    # now run the app and when you navigate to the front page, the output of the query should be there
 
 
 # TODO logout should be a POST request
